a8008c98cd0566b2c83ee313d8532700ec1f2e9a
Maarten Hoogendoorn <maarten@moretea.nl>
2013-03-18 14:50:52 +0100

    Extend example to reproduce error

--- examples/companies_create_and_destroy.rb
+++ examples/companies_create_and_destroy.rb
@@ -3,4 +3,6 @@ require_relative "config"
 
 client = PipelineDealers::Client.new(api_key: YOUR_API_KEY)
 company = client.companies.create(name: "Awesome Company")
+company.name = "blah"
+company.save
 company.destroy
