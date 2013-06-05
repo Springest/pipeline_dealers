6535c8c9a3118ef47877a1fe83c455af62f19035
Maarten Hoogendoorn <maarten@moretea.nl>
2013-06-05 17:05:40 +0200

    Made example output someting to stdout

--- examples/companies_create_and_destroy.rb
+++ examples/companies_create_and_destroy.rb
@@ -4,5 +4,9 @@ require_relative "config"
 client = PipelineDealers::Client.new(api_key: YOUR_API_KEY)
 company = client.companies.create(name: "Awesome Company")
 company.name = "blah"
+
 company.save
+puts "Created company #{company.name}, with id #{company.id}"
+
 company.destroy
+puts "And destroyed it"
